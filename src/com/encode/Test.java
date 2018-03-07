edd72abdfac6ff3024196172e557217286389fe5
leejooho77 <leejooho@uga.edu>
2018-03-07 10:48:40 -0500

    Update Test.java

--- src/com/encode/Test.java
+++ src/com/encode/Test.java
@@ -1,4 +1,11 @@
 package com.encode;
 
 public class Test {
+
+    public static void main(String[] args) {
+        CaesarCipher caesarCipher = new CaesarCipher();
+        String message = "lbh zhfg hayrnea jung lbh unir yrnearq!!!!";
+        System.out.println("Encoded: " + message);
+        System.out.println("Decoded: " + caesarCipher.decode(message, 13));
+    }
 }
